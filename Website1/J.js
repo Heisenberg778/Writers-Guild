7ec3c4a7976f9dc541380f886908efe41a59c450
Heisenberg778 <ayrat778@gmail.com>
2016-03-04 20:53:45 +0200

    photo and floating menu upgrades

--- Website1/J.js
+++ Website1/J.js
@@ -12,5 +12,37 @@
           });
      }
      
+    if(Scroll > $('.sub-header h1').offset().top - ($(window).height()/1.7)){
+        
+        $('#li1').each(function(){
+                       
+                $('#li1').addClass('dot-display');
+                       });
+        $('#li2').each(function(){
+                       
+                $('#li2').removeClass('dot-display');
+                       });
+        
+    }
+     
+          
+    if(Scroll > $('.parallax').offset().top - ($(window).height()/1.7)){
+        
+        $('#li1').each(function(){
+                       
+                $('#li1').removeClass('dot-display');
+                       });
+        
+        $('#li2').each(function(){
+                       
+                $('#li2').addClass('dot-display');
+                       });
+        
+    }
+     
+      
+     
  });
+
+    
     
